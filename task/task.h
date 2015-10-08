ef051a9fdbdc79351d2932817ab7ac41a00c92c6
Mattes Groeger <info@mattes-groeger.de>
2015-10-08 23:44:06 +0200

    Initial Commit

---
+++ task/task.h
@@ -0,0 +1,19 @@
+//
+//  task.h
+//  task
+//
+//  Created by Mattes Groeger on 08/10/15.
+//  Copyright © 2015 Mattes Groeger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for task.
+FOUNDATION_EXPORT double taskVersionNumber;
+
+//! Project version string for task.
+FOUNDATION_EXPORT const unsigned char taskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <task/PublicHeader.h>
+
+
